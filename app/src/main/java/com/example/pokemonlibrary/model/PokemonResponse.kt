3ec0600e15e9c495b813379ce4975c4ea0724903
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-10 10:11:09 +0200

    add PokemonResponse with various fields

---
+++ app/src/main/java/com/example/pokemonlibrary/model/PokemonResponse.kt
@@ -0,0 +1,42 @@
+package com.example.pokemonlibrary.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonResponse(
+    val forms: List<Form>,
+    val height: Int,
+    val stats: List<PokemonStat>,
+    val sprites: Sprites
+)
+
+data class Form(
+    val name: String
+)
+
+data class Sprites(
+    val other: OtherSprites
+)
+
+data class OtherSprites(
+    @SerializedName("official-artwork")
+    val officialArtwork: OfficialArtwork
+)
+
+data class OfficialArtwork(
+    @SerializedName("front_default")
+    val frontDefault: String,
+    @SerializedName("front_shiny")
+    val frontShiny: String
+)
+
+data class PokemonStat(
+    @SerializedName("base_stat")
+    val baseStat: Int,
+    val effort: Int,
+    val stat: StatDetail
+)
+
+data class StatDetail(
+    val name: String,
+    val url: String
+)
