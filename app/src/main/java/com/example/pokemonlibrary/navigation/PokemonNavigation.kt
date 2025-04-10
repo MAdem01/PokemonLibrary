f7c75da5629b4bbf32d7d10a46829a1402cd36bb
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-10 09:18:35 +0200

    add HomeScreen to navigation

--- app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
+++ app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
@@ -4,13 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pokemonlibrary.screens.HomeScreen
 
 @Composable
 fun PokemonNavigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = PokemonScreens.HOME_SCREEN.name){
         composable(PokemonScreens.HOME_SCREEN.name){
-
+            HomeScreen(navController = navController)
         }
         composable(PokemonScreens.POKEMON_SCREEN.name){
 
