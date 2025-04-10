657e82173331267ef4c7c179f2792f46de6c6c19
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-10 09:45:58 +0200

    add PokemonApiService

---
+++ app/src/main/java/com/example/pokemonlibrary/data/PokemonApiService.kt
@@ -0,0 +1,9 @@
+package com.example.pokemonlibrary.data
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PokemonApiService{
+    @GET("pokemon/{id}")
+    suspend fun getPokemon(@Path("id") id: Int): PokemonResponse
+}
