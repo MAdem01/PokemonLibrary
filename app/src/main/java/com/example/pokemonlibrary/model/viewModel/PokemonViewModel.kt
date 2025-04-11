595a318fcc60e42f99479ae7e81802916b885300
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-11 09:33:26 +0200

    get pokemon evolution data and refactor to more modular code

--- app/src/main/java/com/example/pokemonlibrary/model/viewModel/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokemonlibrary/model/viewModel/PokemonViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.pokemonlibrary.model.viewModel
 
-import androidx.compose.runtime.LaunchedEffect
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokemonlibrary.data.repository.PokemonRepository
+import com.example.pokemonlibrary.model.EvolutionData
 import com.example.pokemonlibrary.model.PokemonEvolutionChainResponse
 import com.example.pokemonlibrary.model.PokemonResponse
 import com.example.pokemonlibrary.model.PokemonSpeciesResponse
@@ -23,30 +24,73 @@ class PokemonViewModel(private val pokemonRepository: PokemonRepository): ViewMo
     private val _pokemonEvolutionChainData: MutableStateFlow<PokemonEvolutionChainResponse?> = MutableStateFlow(null)
     val pokemonEvolutionChainData: StateFlow<PokemonEvolutionChainResponse?> get() = _pokemonEvolutionChainData
 
+    private val _pokemonEvolutionChainImages: MutableStateFlow<List<String>?> = MutableStateFlow(null)
+    val pokemonEvolutionChainImages: StateFlow<List<String>?> get() = _pokemonEvolutionChainImages
+
+
     init {
-        fetchPokemonData(Random.nextInt(1, 1302))
+        fetchPokemonData(Random.nextInt(1, 1025))
     }
 
     fun fetchPokemonData(id: Int){
         viewModelScope.launch {
             try{
-                val pokemonData = pokemonRepository.getPokemon(id)
-                _pokemonData.value = pokemonData
+                val pokemonData = fetchPokemon(id)
+                pokemonData.species.url.let { speciesUrl ->
+                    val pokemonSpeciesData = fetchPokemonSpeciesData(speciesUrl)
 
-                pokemonData?.species?.url?.let { speciesUrl ->
-                    val pokemonSpeciesData = pokemonRepository.getPokemonSpecies(speciesUrl)
-                    _pokemonSpeciesData.value = pokemonSpeciesData
-
-                    pokemonSpeciesData.evolutionChain?.url?.let { evolutionChainUrl ->
-                        val pokemonEvolutionChainData = pokemonRepository.getPokemonEvolutionChain(evolutionChainUrl)
-                        _pokemonEvolutionChainData.value = pokemonEvolutionChainData
+                    pokemonSpeciesData.evolutionChain.url.let { evolutionChainUrl ->
+                        fetchPokemonEvolutionData(evolutionChainUrl)
                     }
                 }
-
 
             }catch (e: Exception){
                 throw Exception("Error fetching the data: $e")
             }
         }
     }
+
+    private suspend fun fetchPokemon(id: Int): PokemonResponse{
+        Log.d("ppp", "$id")
+        val pokemonData = pokemonRepository.getPokemonById(id)
+        _pokemonData.value = pokemonData
+        return pokemonData
+    }
+
+    private suspend fun fetchPokemonSpeciesData(url: String): PokemonSpeciesResponse{
+        Log.d("ppp", "$url")
+        val pokemonSpeciesData = pokemonRepository.getPokemonSpecies(url)
+        _pokemonSpeciesData.value = pokemonSpeciesData
+        return pokemonSpeciesData
+    }
+
+    private suspend fun fetchPokemonEvolutionData(url: String){
+        Log.d("ppp", "$url")
+        val pokemonEvolutionData = pokemonRepository.getPokemonEvolutionChain(url)
+        _pokemonEvolutionChainData.value = pokemonEvolutionData
+        fetchPokemonEvolutionImages(pokemonEvolutionData)
+    }
+
+    private suspend fun fetchPokemonEvolutionImages(pokemonEvolutionChainData: PokemonEvolutionChainResponse){
+        Log.d("ppp", "${pokemonEvolutionChainData.evolutionChain}")
+        _pokemonEvolutionChainImages.value =
+            getAllPokemonNames(pokemonEvolutionChainData.evolutionChain).map { name ->
+                val pokemon = pokemonRepository.getPokemonByName(name)
+                pokemon.sprites.other.officialArtwork.frontShiny
+            }
+    }
+
+    private fun getAllPokemonNames(evolutionData: EvolutionData?): List<String> {
+        val pokemonNames = mutableListOf<String>()
+        if (evolutionData?.species == null) return pokemonNames
+
+        evolutionData.species.name.let { pokemonNames.add(it) }
+
+        evolutionData.evolvesTo.forEach { evolve ->
+            pokemonNames.addAll(getAllPokemonNames(EvolutionData(evolve.species, evolve.evolvesTo)))
+        }
+
+        return pokemonNames
+    }
 }
+
