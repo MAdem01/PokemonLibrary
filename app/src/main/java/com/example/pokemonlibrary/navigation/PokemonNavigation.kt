70c75c60148b24e40284d559944bec7b41063e97
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-11 15:26:04 +0200

    add AboutScreen.kt to PokemonNavigation.kt and move PokemonViewModel to navigation

--- app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
+++ app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
@@ -1,20 +1,32 @@
 package com.example.pokemonlibrary.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pokemonlibrary.data.repository.PokemonRepository
+import com.example.pokemonlibrary.data.retrofitClient.RetroFitClient
+import com.example.pokemonlibrary.model.viewModel.PokemonViewModel
+import com.example.pokemonlibrary.model.viewModel.PokemonViewModelFactory
+import com.example.pokemonlibrary.screens.AboutScreen
 import com.example.pokemonlibrary.screens.HomeScreen
 
 @Composable
 fun PokemonNavigation(){
+    val pokemonService = RetroFitClient.api
+    val pokemonRepository = PokemonRepository(pokemonService)
+    val pokemonViewModel: PokemonViewModel = viewModel(
+        factory = PokemonViewModelFactory(pokemonRepository)
+    )
+
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = PokemonScreens.HOME_SCREEN.name){
         composable(PokemonScreens.HOME_SCREEN.name){
-            HomeScreen(navController = navController)
+            HomeScreen(navController = navController, pokemonViewModel = pokemonViewModel)
         }
-        composable(PokemonScreens.POKEMON_SCREEN.name){
-
+        composable(PokemonScreens.ABOUT_SCREEN.name){
+            AboutScreen(navController = navController, pokemonViewModel = pokemonViewModel)
         }
         composable(PokemonScreens.FAVOURITES_SCREEN.name){
 
