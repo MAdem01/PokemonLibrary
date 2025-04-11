ade2508368a95d87f3ca1cb7444c16ec7970ffdb
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-11 10:34:07 +0200

    add BottomAppBar to Screen

--- app/src/main/java/com/example/pokemonlibrary/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pokemonlibrary/screens/HomeScreen.kt
@@ -1,14 +1,14 @@
 package com.example.pokemonlibrary.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -16,24 +16,22 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.pokemonlibrary.R
 import com.example.pokemonlibrary.data.repository.PokemonRepository
 import com.example.pokemonlibrary.data.retrofitClient.RetroFitClient
 import com.example.pokemonlibrary.model.viewModel.PokemonViewModel
 import com.example.pokemonlibrary.model.viewModel.PokemonViewModelFactory
+import com.example.pokemonlibrary.widgets.BottomBarContent
 import com.example.pokemonlibrary.widgets.EvolutionChain
 import com.example.pokemonlibrary.widgets.PokemonImageRow
 import com.example.pokemonlibrary.widgets.PokemonStatCard
-import com.example.pokemonlibrary.widgets.TopBarContent
 
 @Preview
 @Composable
@@ -51,8 +49,14 @@ fun HomeScreen(navController: NavController = rememberNavController()){
     Box{
         Scaffold(
             bottomBar = {
-                BottomAppBar{
-                    TopBarContent()
+                BottomAppBar(
+                    modifier = Modifier
+                        .height(60.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(20.dp)),
+                    containerColor = Color.White
+                ){
+                    BottomBarContent()
                 }
             }
         ) { paddingValues ->
@@ -66,16 +70,5 @@ fun HomeScreen(navController: NavController = rememberNavController()){
                 }
             }
         }
-        HorizontalDivider(
-            modifier = Modifier.padding(top = 55.dp),
-            color = Color.Black,
-            thickness = 5.dp)
-        Image(
-            painter = painterResource(id = R.drawable.poke_ball),
-            contentDescription = "PokeBall Image",
-            modifier = Modifier
-                .align(alignment = Alignment.TopCenter)
-                .padding(top = 12.dp)
-            )
     }
 }
