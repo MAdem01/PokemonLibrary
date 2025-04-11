1ce5293bdf68d893544585586c68f6f9ff844c7b
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-11 14:48:33 +0200

    add color field for data fetching

--- app/src/main/java/com/example/pokemonlibrary/model/PokemonSpeciesResponse.kt
+++ app/src/main/java/com/example/pokemonlibrary/model/PokemonSpeciesResponse.kt
@@ -4,9 +4,14 @@ import com.google.gson.annotations.SerializedName
 
 data class PokemonSpeciesResponse(
     @SerializedName("evolution_chain")
-    val evolutionChain: EvolutionChain
+    val evolutionChain: EvolutionChain,
+    val color: Color
 )
 
 data class EvolutionChain(
     val url: String
+)
+
+data class Color(
+    val name: String
 )
