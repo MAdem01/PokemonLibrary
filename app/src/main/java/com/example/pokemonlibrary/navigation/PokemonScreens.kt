8675439fbd6a6554e9a86b0e0c086501bf1c7dde
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-11 15:25:35 +0200

    add AboutScreen.kt to PokemonScreens

--- app/src/main/java/com/example/pokemonlibrary/navigation/PokemonScreens.kt
+++ app/src/main/java/com/example/pokemonlibrary/navigation/PokemonScreens.kt
@@ -2,13 +2,13 @@ package com.example.pokemonlibrary.navigation
 
 enum class PokemonScreens{
     HOME_SCREEN,
-    POKEMON_SCREEN,
+    ABOUT_SCREEN,
     FAVOURITES_SCREEN;
     companion object{
         fun fromRoute(route: String?){
             when(route?.substringBefore("/")){
                 HOME_SCREEN.name -> HOME_SCREEN
-                POKEMON_SCREEN.name -> POKEMON_SCREEN
+                ABOUT_SCREEN.name -> ABOUT_SCREEN
                 FAVOURITES_SCREEN.name -> FAVOURITES_SCREEN
                 null -> HOME_SCREEN
                 else -> throw IllegalArgumentException("Route $route is not recognized")
