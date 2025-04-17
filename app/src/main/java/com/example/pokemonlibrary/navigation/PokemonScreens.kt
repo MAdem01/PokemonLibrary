8be2c2c045a86ba5252e56472c71b422696f3f58
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-17 13:58:15 +0200

    add PokemonScreen to navigation

--- app/src/main/java/com/example/pokemonlibrary/navigation/PokemonScreens.kt
+++ app/src/main/java/com/example/pokemonlibrary/navigation/PokemonScreens.kt
@@ -3,12 +3,14 @@ package com.example.pokemonlibrary.navigation
 enum class PokemonScreens{
     HOME_SCREEN,
     ABOUT_SCREEN,
+    POKEMON_SCREEN,
     FAVOURITES_SCREEN;
     companion object{
         fun fromRoute(route: String?){
             when(route?.substringBefore("/")){
                 HOME_SCREEN.name -> HOME_SCREEN
                 ABOUT_SCREEN.name -> ABOUT_SCREEN
+                POKEMON_SCREEN.name -> POKEMON_SCREEN
                 FAVOURITES_SCREEN.name -> FAVOURITES_SCREEN
                 null -> HOME_SCREEN
                 else -> throw IllegalArgumentException("Route $route is not recognized")
