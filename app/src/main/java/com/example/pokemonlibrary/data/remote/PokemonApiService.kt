f16b882a7fbb7e6739a1bfd562ab8bd15b7eef0f
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-17 15:22:19 +0200

    create endpoint for getting pokemons

--- app/src/main/java/com/example/pokemonlibrary/data/remote/PokemonApiService.kt
+++ app/src/main/java/com/example/pokemonlibrary/data/remote/PokemonApiService.kt
@@ -1,12 +1,21 @@
 package com.example.pokemonlibrary.data.remote
 
 import com.example.pokemonlibrary.model.PokemonEvolutionChainResponse
+import com.example.pokemonlibrary.model.PokemonListResponse
 import com.example.pokemonlibrary.model.PokemonResponse
 import com.example.pokemonlibrary.model.PokemonSpeciesResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PokemonApiService{
+
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): PokemonListResponse
+
     @GET("pokemon/{id}")
     suspend fun getPokemonById(@Path("id") id: Int): PokemonResponse
 
