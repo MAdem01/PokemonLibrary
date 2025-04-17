de575d1b0913aefecba31c522fa72ee5cf85575e
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-17 15:23:25 +0200

    add PokemonsViewModel to navigation

--- app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
+++ app/src/main/java/com/example/pokemonlibrary/navigation/PokemonNavigation.kt
@@ -7,8 +7,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pokemonlibrary.data.repository.PokemonRepository
 import com.example.pokemonlibrary.data.retrofitClient.RetroFitClient
-import com.example.pokemonlibrary.model.viewModel.PokemonViewModel
-import com.example.pokemonlibrary.model.viewModel.PokemonViewModelFactory
+import com.example.pokemonlibrary.model.viewModel.pokemonViewModel.PokemonViewModel
+import com.example.pokemonlibrary.model.viewModel.pokemonViewModel.PokemonViewModelFactory
+import com.example.pokemonlibrary.model.viewModel.pokemonsViewModel.PokemonsViewModel
+import com.example.pokemonlibrary.model.viewModel.pokemonsViewModel.PokemonsViewModelFactory
 import com.example.pokemonlibrary.screens.AboutScreen
 import com.example.pokemonlibrary.screens.HomeScreen
 import com.example.pokemonlibrary.screens.PokemonScreen
@@ -20,6 +22,9 @@ fun PokemonNavigation(){
     val pokemonViewModel: PokemonViewModel = viewModel(
         factory = PokemonViewModelFactory(pokemonRepository)
     )
+    val pokemonsViewModel: PokemonsViewModel = viewModel(
+        factory = PokemonsViewModelFactory(pokemonRepository)
+    )
 
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = PokemonScreens.HOME_SCREEN.name){
@@ -30,7 +35,7 @@ fun PokemonNavigation(){
             AboutScreen(navController = navController, pokemonViewModel = pokemonViewModel)
         }
         composable(PokemonScreens.POKEMON_SCREEN.name){
-            PokemonScreen(navController = navController)
+            PokemonScreen(navController = navController, pokemonsViewModel = pokemonsViewModel)
         }
         composable(PokemonScreens.FAVOURITES_SCREEN.name){
 
