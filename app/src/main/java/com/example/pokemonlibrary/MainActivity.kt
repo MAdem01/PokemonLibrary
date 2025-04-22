74bdc9f8d065f0f0b4709aac9a2ec009fead69ea
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-22 17:33:15 +0200

    add preloadDatabaseIfNeeded fun on app start

--- app/src/main/java/com/example/pokemonlibrary/MainActivity.kt
+++ app/src/main/java/com/example/pokemonlibrary/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pokemonlibrary.model.room.PokemonDatabase
 import com.example.pokemonlibrary.navigation.PokemonNavigation
 import com.example.pokemonlibrary.ui.theme.PokemonLibraryTheme
+import com.example.pokemonlibrary.utils.preloadDatabaseIfNeeded
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        preloadDatabaseIfNeeded(this, PokemonDatabase.getDatabase(this))
         enableEdgeToEdge()
         setContent {
             MyApp {
