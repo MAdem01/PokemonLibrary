3f85090aa4617cbb103d4430dd742fa63de0205c
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-22 17:31:54 +0200

    add PokemonRepository

--- app/src/main/java/com/example/pokemonlibrary/data/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/pokemonlibrary/data/repository/PokemonRepository.kt
@@ -1,35 +1,25 @@
 package com.example.pokemonlibrary.data.repository
 
-import com.example.pokemonlibrary.data.remote.PokemonApiService
-import com.example.pokemonlibrary.model.PokemonEvolutionChainResponse
-import com.example.pokemonlibrary.model.PokemonListResponse
-import com.example.pokemonlibrary.model.PokemonResponse
-import com.example.pokemonlibrary.model.PokemonSpeciesResponse
+import com.example.pokemonlibrary.model.room.PokemonDao
+import com.example.pokemonlibrary.model.room.PokemonEntity
 
-class PokemonRepository(private val apiService: PokemonApiService) {
+class PokemonRepository(private val dao: PokemonDao) {
 
-    suspend fun getPokemons(limit: Int, offset: Int): PokemonListResponse{
-        return apiService.getPokemonList(limit = limit, offset = offset)
+    fun getPokemonByName(name: String): PokemonEntity {
+        return dao.getPokemonByName(name)
     }
 
-    suspend fun getPokemonById(id: Int): PokemonResponse{
-       return apiService.getPokemonById(id)
+    fun getPokemons(offset: Int): List<PokemonEntity> {
+        return dao.getPokemons(offset)
     }
 
-    suspend fun getPokemonSpecies(url: String): PokemonSpeciesResponse{
-        return apiService.getPokemonSpecies(extractIdFromUrl(url))
+    fun insertAll(pokemons: List<PokemonEntity>) {
+        dao.insertAll(pokemons)
     }
 
-    suspend fun getPokemonEvolutionChain(url: String): PokemonEvolutionChainResponse{
-        return apiService.getPokemonEvolutionChain(extractIdFromUrl(url))
+    fun getRandomPokemon(): PokemonEntity {
+       return dao.getRandomPokemon()
     }
 
-    suspend fun getPokemonByName(name: String): PokemonResponse{
-        return apiService.getPokemonByName(name)
-    }
-
-    private fun extractIdFromUrl(url: String): Int {
-        val parts = url.split("/")
-        return parts[parts.size - 2].toInt()
-    }
+    fun getPokemonCount(): Int = dao.getPokemonCount()
 }
